4791174593a3c09a7a72a72e10985f4747be2d4e
Anurag Joshi <anurag_joshi@outlook.com>
2020-10-01 00:41:48 +0530

    Updated whitespaces

--- 11.php
+++ 11.php
@@ -6,12 +6,12 @@ function check($txt1,$txt2){
     else
         echo $txt1."-does not contain string-".$txt2;
 }
-$str1="How are you my friend !";
+$str1="How are you my friend!";
 $str2="my friend";
 $str3="hello";
-echo "String 1 : ".$str1."\n";
-echo "String 2 : ".$str2."\n";
-echo "String 3 : ".$str3."\n";
+echo "String 1: ".$str1."\n";
+echo "String 2: ".$str2."\n";
+echo "String 3: ".$str3."\n";
 check($str1,$str2);
 check($str1,$str3);
 ?>
